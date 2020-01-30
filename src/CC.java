5bb958159c5a688a36f7c6eb5adad8198b9c04d2
sandromuggli <sandro.muggli@gmail.com>
2020-01-30 08:12:48 +0100

    use lamba and other crazy java8 stuff:P

--- src/CC.java
+++ src/CC.java
@@ -1,6 +1,7 @@
 import com.sun.tools.javac.util.StringUtils;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class CC {
 
@@ -10,15 +11,13 @@ public class CC {
 
     //+1
     public final static void printOneCharacterOnEachLine(final List<String> chars) {
-        //+1
-        if (chars == null) {
-            return;
-        }
-
-        List<String> charsCopy = new ArrayList<>(chars);
-        charsCopy.removeAll(Arrays.asList(null, ""));
-
-        charsCopy.forEach(System.out::println); // +1
+        Optional.ofNullable(chars)
+                .map(Collection::stream)
+                .orElseGet(Stream::empty)
+                .filter(Objects::nonNull)
+                //+1
+                .filter(item -> !item.isEmpty())
+                .forEach(System.out::println);
     }
 }
 
